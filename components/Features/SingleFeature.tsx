58e0b2aa2c664bcefd7fd9c95846339173cb0f33
Omkar Deshmukh <omkardeshmukh7890@gmail.com>
2025-02-25 19:15:08 +0530

    fixed flickering of feature section

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const SingleFeature = ({ feature }: { feature: Feature }) => {
   const { icon, title, description } = feature;
-
+  
   return (
     <>
       <motion.div
@@ -14,7 +14,6 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
             opacity: 0,
             y: -10,
           },
-
           visible: {
             opacity: 1,
             y: 0,
@@ -23,11 +22,11 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         initial="hidden"
         whileInView="visible"
         transition={{ duration: 0.5 }}
-        viewport={{ once: true }}
+        viewport={{ once: true, margin: "-100px" }}
         className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark xl:p-12.5"
       >
         <div className="relative flex h-16 w-16 items-center justify-center rounded-[4px] bg-primary">
-          <Image src={icon} width={36} height={36} alt="title" />
+          <Image src={icon} width={36} height={36} alt={title} />
         </div>
         <h3 className="mb-5 mt-7.5 text-xl font-semibold text-black dark:text-white xl:text-itemtitle">
           {title}

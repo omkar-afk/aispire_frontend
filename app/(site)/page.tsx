3bf3fc7f994dec2aa3209d185554d76e5ce59490
Omkar Deshmukh <omkardeshmukh7890@gmail.com>
2025-02-26 16:11:57 +0530

    changed metadata

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,10 +14,10 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  title: "AIspire Labs",
 
   // other metadata
-  description: "This is Home for Solid Pro"
+  // description: "This is Home for Solid Pro"
 };
 
 export default function Home() {

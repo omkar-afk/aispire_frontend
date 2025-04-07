702af3920c4fcf556a72a27ec1061f7af50268f1
Omkar Deshmukh <omkardeshmukh7890@gmail.com>
2025-04-08 00:11:15 +0530

    chagnes

--- components/Features/FeatureCard.tsx
+++ components/Features/FeatureCard.tsx
@@ -16,13 +16,13 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({
-  i, 
-  title, 
-  description, 
-  src, 
-  color, 
-  progress, 
-  range, 
+  i,
+  title,
+  description,
+  src,
+  color,
+  progress,
+  range,
   targetScale
 }) => {
   const container = useRef(null);
@@ -30,74 +30,71 @@ const Card: React.FC<CardProps> = ({
     target: container,
     offset: ['start end', 'start start']
   })
-
+  
   // Improved easing for smoother scale transitions
   const imageScale = useTransform(
-    scrollYProgress, 
-    [0, 1], 
-    [2, 1], 
+    scrollYProgress,
+    [0, 1],
+    [2, 1],
     { ease: (t) => 1 - Math.pow(1 - t, 4) } // Smooth cubic easing
   )
   
   // More gradual scale transformation
   const scale = useTransform(
-    progress, 
-    range, 
-    [1, targetScale], 
+    progress,
+    range,
+    [1, targetScale],
     { ease: (t) => 1 - Math.pow(1 - t, 3) } // Soft deceleration
   );
- 
+
   return (
     <div ref={container} className={styles.cardContainer}>
-      <motion.div 
+      <motion.div
         style={{
-          backgroundColor: color, 
-          scale, 
-          top: `calc(-5vh + ${i * 25}px)`,
-          transition: {
-            type: 'spring',
-            stiffness: 100,
-            damping: 20
-          }
-        }} 
+          backgroundColor: color,
+          scale,
+          top: `calc(-5vh + ${i * 25}px)`
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 20
+        }}
         className={styles.card}
       >
         <h2>{title}</h2>
         <div className={styles.body}
-        style={!src ? {
-          width: '100%',
-          top: '0',
-          textAlign: 'center',
-
-          display: 'flex',
-          justifyContent:'space-around',
-        } : {}}>
-          <div 
-            className={styles.description} 
-            
+          style={!src ? {
+            width: '100%',
+            top: '0',
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent:'space-around',
+          } : {}}>
+          <div
+            className={styles.description}
           >
             <p>{description}</p>
             <span>
               {/* Commented out links/icons */}
             </span>
           </div>
-          
           {src && (
             <div className={styles.imageContainer}>
               <motion.div
                 className={styles.inner}
                 style={{
-                  scale: imageScale,
-                  transition: {
-                    type: 'spring',
-                    stiffness: 80,
-                    damping: 15
-                  }
+                  scale: imageScale
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 80,
+                  damping: 15
                 }}
               >
                 <Image
                   src={`/images/${src}`}
-                  alt="image" 
+                  alt="image"
                   fill
                   style={{objectFit: "contain"}}
                 />
